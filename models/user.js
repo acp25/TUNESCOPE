af91449b88b989a337af23e7307c1eb790caf940
Jared Colletti <JaredColletti123@gmail.com>
2021-07-16 10:36:23 -0400

    added user model and playlist model

---
+++ models/user.js
@@ -0,0 +1,71 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+// Initialize Product model (table) by extending off Sequelize's Model class
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [10],
+            },
+            
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true
+            },
+        },
+        playlist_id: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            references: {
+                model: "Playlist",
+                key: "id"
+            },
+            
+        }
+        
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'User',
+    }
+    );
+    
+    module.exports = User;
