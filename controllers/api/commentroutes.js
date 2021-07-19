fb447d3c5c639269511e7d6b829412607b10a916
Drake Gooding <dgooding1008@gmail.com>
2021-07-19 09:34:30 -0500

    Comment posting works

---
+++ controllers/api/commentroutes.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const { Comment } = require("../../models");
+
+
+router.get("/", async (req, res) => {
+    try {
+        const commentData = await Comment.findAll({})
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+
+router.post("/", async (req, res) => {
+    try {
+      const newComment = await Comment.create({
+        ...req.body
+      });
+      res.status(200).json(newComment);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  
+  });
+
+  module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const albumRoutes = require('./albumroutes');
 const playlistRoutes = require('./playlistroutes');
+const commentRoutes = require('./commentroutes')
 
 router.use('/albums', albumRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
